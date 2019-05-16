c16079c9f19802acb3a540c80b89c4e1c291f9bb
Jørgen Stamnes <jorghs@stud.ntnu.no>
2019-05-16 09:17:57 +0200

    Add translations liibrary I18n

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { I18n } from "@aws-amplify/core";
+import logo from "./logo.svg";
+import dict from "./AppI18n";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello NTNUI Calisthenics
-        </p>
+        <p>{I18n.get("Hello NTNUI Calisthenics")}</p>
+        <button onClick={() => I18n.setLanguage("en")}>
+          {I18n.get("Change to english")}
+        </button>
+        <button onClick={() => I18n.setLanguage("no")}>
+          {I18n.get("Change to norwegian")}
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -22,5 +28,8 @@ function App() {
     </div>
   );
 }
+
+I18n.putVocabularies(dict);
+I18n.setLanguage("no");
 
 export default App;
