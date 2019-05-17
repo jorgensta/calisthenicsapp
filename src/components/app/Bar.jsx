3a5af27e54915a6202a788993d71acc8c6a8d82b
Jørgen Stamnes <jorghs@stud.ntnu.no>
2019-05-17 08:09:02 +0200

    Preparing tabs

--- src/components/app/Bar.jsx
+++ src/components/app/Bar.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-import { withStyles } from "@material-ui/core/styles";
+import withWidth from '@material-ui/core/withWidth';
 
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -11,53 +11,145 @@ import MenuIcon from "@material-ui/icons/Menu";
 
 import titleImage from "../../assets/NTNUI_TRYKK.png";
 
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-    opacity: 0.9
-  },
-  grow: {
-    flexGrow: 1,
-    color: "white"
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20
-  },
-  logo: {
-    height: 100,
-    width: 120,
-    margin: 10,
-    marginLeft: 50
-  }
-});
+import SwipeableViews from 'react-swipeable-views';
+import { makeStyles, useTheme } from '@material-ui/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
-const ButtonAppBar = ({ classes }) => (
-  <div className={classes.root}>
-    <AppBar position="static">
-      <Toolbar>
-        <img
-          src={titleImage}
-          alt="Calisthenics logo"
-          className={classes.logo}
-        />
-        <Typography variant="h6" color="white" className={classes.grow}>
-          NTNUI Calisthenics
-        </Typography>
-        <IconButton
-          className={classes.menuButton}
-          color="inherit"
-          aria-label="Menu"
-        >
-          <MenuIcon fontSize="large" />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
-  </div>
-);
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+        opacity: 0.9
+    },
+    grow: {
+        flexGrow: 1,
+        color: "white"
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20
+    },
+    logo: {
+        height: 100,
+        width: 120,
+        margin: 10,
+        marginLeft: 50
+    }
+}));
+
+/* 
+xs, extra - small: 
+sm, small: 600px or larger
+md, medium: 960px or larger
+lg, large: 1280px or larger
+xl, extra - large: 1920px or larger
+
+*/
+
+const toolbarComponentHandler = width => {
+    switch (width) {
+        case 'xs':
+            return <div>XS: 0px or larger</div>
+        case 'sm':
+            return <div>SM: 600px or larger</div>
+        case 'md':
+            return <div>MD: 960px or larger</div>
+        case 'lg':
+            return <div>LG: 1280px or larger</div>
+        case 'xl':
+            return <div>XL: 1920px or larger</div>
+    }
+}
+
+const ButtonAppBar = ({ width }) => {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => setValue(newValue);
+    const handleChangeIndex = index = setValue(index);
+    
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <img
+                        src={titleImage}
+                        alt="Calisthenics logo"
+                        className={classes.logo}
+                    />
+                    <Typography variant="h6" color="white" className={classes.grow}>
+                        {toolbarComponentHandler(width)}
+                    </Typography>
+                    <IconButton
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="Menu"
+                    >
+                        <MenuIcon fontSize="large" />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
 
 ButtonAppBar.propTypes = {
-  classes: PropTypes.shape({}).isRequired
+    classes: PropTypes.shape({}).isRequired,
+    width: PropTypes.string.isRequired
 };
 
-export default withStyles(styles)(ButtonAppBar);
+const TabContainer = ({ children, dir }) => (
+    <Typography component="div" dir={dir} style={{ padding: 8 * 3 }}>
+        {children}
+    </Typography>
+);
+
+TabContainer.propTypes = {
+    children: PropTypes.node.isRequired,
+    dir: PropTypes.string.isRequired,
+};
+
+
+function FullWidthTabs() {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [value, setValue] = React.useState(0);
+
+    function handleChange(event, newValue) {
+        setValue(newValue);
+    }
+
+    function handleChangeIndex(index) {
+        setValue(index);
+    }
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static" color="default">
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    variant={null}
+                >
+                    <Tab label="Item One" />
+                    <Tab label="Item Two" />
+                    <Tab label="Item Three" />
+                </Tabs>
+            </AppBar>
+            <SwipeableViews
+                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                index={value}
+                onChangeIndex={handleChangeIndex}
+            >
+                <TabContainer dir={theme.direction}>Item One</TabContainer>
+                <TabContainer dir={theme.direction}>Item Two</TabContainer>
+                <TabContainer dir={theme.direction}>Item Three</TabContainer>
+            </SwipeableViews>
+        </div>
+    );
+}
+
+export default withWidth()(ButtonAppBar);
--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => (
   <div>
-    <h1>HOME</h1>
+    <h1 style={{color: 'white'}}>HOME</h1>
   </div>
 );
 
